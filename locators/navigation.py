1e3c0d19894dd26e2c5131038571716a030c4bbf
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-15 15:02:02 +0700

    Add: Navigation Component

---
+++ locators/navigation.py
@@ -0,0 +1,10 @@
+class Locator:
+    linkShoppingCartXPath = "//a[@class='shopping_cart_link']"
+
+    buttonOpenBurgerMenuXPath = "//button[@id='react-burger-menu-btn']"
+    buttonCloseBurgerMenuXPath = "//button[@id='react-burger-cross-btn']"
+
+    linkInventoryXPath = "//*[@id='inventory_sidebar_link']"
+    linkAboutXPath = "//*[@id='about_sidebar_link']"
+    linkLogoutXPath = "//*[@id='logout_sidebar_link']"
+    linkResetXPath = "//*[@id='reset_sidebar_link']"
---
+++ pages/navigation.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from locators.navigation import Locator
+
+class NavBar:
+    pass
