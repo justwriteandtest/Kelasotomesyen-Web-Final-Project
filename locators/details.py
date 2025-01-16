0d7d7779de222a94d69ebabfb56fe07b38bd8097
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-16 16:15:06 +0700

    Add!: Element locators for the item details page

---
+++ locators/details.py
@@ -0,0 +1,4 @@
+class Locator:
+    buttonAddToCartXPath = "//button[@id='add-to-cart']"
+    buttonRemoveFromCartXPath = "//button[@id='remove']"
+    buttonBackXPath = "//button[@id='back-to-products']"
