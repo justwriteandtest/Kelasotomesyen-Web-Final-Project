2842852cfb841d153898879dfc6d99a8ce595ae4
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-16 15:48:37 +0700

    Add:const module (usage: constant variables)

---
+++ const/cart.py
@@ -0,0 +1,10 @@
+class AddToCartMethods:
+    doNotAdd = 0
+    inventoryPage = 1
+    detailsPage = 2
+
+class RemoveFromCartMethods:
+    doNotRemove = 0
+    inventoryPage = 1
+    detailsPage = 2
+    cartPage = 3
---
+++ const/inventory.py
@@ -0,0 +1,11 @@
+class ItemCodes:
+    itemBackpack = "sauce-labs-backpack"
+    itemBikeLight = "sauce-labs-bike-light"
+    itemBoltTShirt = "sauce-labs-bolt-t-shirt"
+    itemFleeceJacket = "sauce-labs-fleece-jacket"
+    itemOnesie = "sauce-labs-onesie"
+    itemTestAllTheThingsTShirt = "test.allthethings()-t-shirt-(red)"
+
+class OpenDetailsMethods:
+    text = 'text'
+    image = 'image'
