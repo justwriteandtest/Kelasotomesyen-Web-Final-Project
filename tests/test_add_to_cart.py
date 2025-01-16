4d0e546d1fa824d2431345a40e3314c1a3881a2f
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-16 16:25:28 +0700

    adjust: `test_add_to_cart` now performs testing item additions to and removals from cart

--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -6,15 +6,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.login import LoginPage
 from pages.inventory import InventoryPage
+from pages.navigation import NavBar
+from pages.cart import CartPage
+from pages.details import ItemDetailsPage
+
+from data.inventory import addToCartList
+
+from const.cart import AddToCartMethods
+from const.cart import RemoveFromCartMethods
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv(override = True)
 
-def testAddToCart(browser : webdriver.Chrome | webdriver.Edge | webdriver.Firefox | webdriver.Safari):
+@pytest.mark.parametrize("itemList, addToCartMethod, removeFromCartMethod", addToCartList)
+def testAddToCart(browser : webdriver.Chrome | webdriver.Edge | webdriver.Firefox | webdriver.Safari, itemList, addToCartMethod, removeFromCartMethod):
     login = LoginPage(browser)
     inventory = InventoryPage(browser)
+    navbar = NavBar(browser)
+    cart = CartPage(browser)
+    details = ItemDetailsPage(browser)
     
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")    
@@ -41,3 +53,54 @@ def testAddToCart(browser : webdriver.Chrome | webdriver.Edge | webdriver.Firefo
         print(e)
 
         assert False
+
+    cartCheck = {}
+
+    for item in itemList.keys():
+        print(f"Adding {item} to cart...")
+
+        match addToCartMethod[item]:
+            case AddToCartMethods.detailsPage:
+                inventory.goToPage(item)
+                details.addToCart()
+                details.backToProducts()
+
+            case AddToCartMethods.inventoryPage:
+                inventory.addToCart(item)
+
+            case _:
+                pass
+        
+        print(f"{item} successfully added to cart.")
+        
+        cartCheck[item] = False
+
+    for item in itemList.keys():
+        match removeFromCartMethod[item]:
+            case RemoveFromCartMethods.detailsPage:
+                inventory.goToPage(item)
+                details.removeFromCart()
+                details.backToProducts()
+
+            case RemoveFromCartMethods.inventoryPage:
+                inventory.removeFromCart(item)
+
+            case RemoveFromCartMethods.cartPage:
+                navbar.goToCart()
+                cart.removeItemFromCart(item)
+                cart.continueShopping()
+
+            case _:
+                pass        
+
+    navbar.goToCart()
+
+    for item in itemList:
+        cartCheck[item] = cart.itemIsInCart(item)
+
+    print(cartCheck)
+    print(itemList)
+
+    assert cartCheck == itemList
+
+    print("Assertion Successful")
