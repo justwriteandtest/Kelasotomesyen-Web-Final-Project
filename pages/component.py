141229f5a169fa02c1151c17f7d2ad994ae549d5
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-16 16:19:45 +0700

    Adjust: Component classes can find elements by xpath

--- pages/component.py
+++ pages/component.py
@@ -4,4 +4,6 @@ from selenium import webdriver
 class Component:
     def __init__(self, driver : webdriver.Chrome | webdriver.Firefox | webdriver.Edge | webdriver.Safari):
         self.driver = driver
-    
+
+    def findElementByXPath(self, xpath = ""):
+        return self.driver.find_element(By.XPATH, xpath)
