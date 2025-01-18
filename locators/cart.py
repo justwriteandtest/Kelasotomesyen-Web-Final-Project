bcd7223ab7e4d380b21b60141c6fddc45bdde1b0
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-01-18 07:01:51 +0700

    Adjust: add item link (and element) locators

--- locators/cart.py
+++ locators/cart.py
@@ -12,3 +12,15 @@ class Locator:
         ItemCodes.itemTestAllTheThingsTShirt : "//button[@id='remove-test.allthethings()-t-shirt-(red)']",
         ItemCodes.itemOnesie : "//button[@id='remove-sauce-labs-onesie']",
     }
+
+    textItemLinkXPath = {
+        ItemCodes.itemBackpack: "//a[@id='item_4_title_link']",
+        ItemCodes.itemBikeLight: "//a[@id='item_0_title_link']",
+        ItemCodes.itemBoltTShirt: "//a[@id='item_1_title_link']",
+        ItemCodes.itemFleeceJacket: "//a[@id='item_5_title_link']",
+        ItemCodes.itemOnesie: "//a[@id='item_2_title_link']",
+        ItemCodes.itemTestAllTheThingsTShirt: "//a[@id='item_3_title_link']",
+    }
+
+    textItemPriceXPath = "{}/../div[@class='item_pricebar']/div[@data-test='inventory-item-price']"
+    textItemQtyXPath = "{}/../../div[@class='cart_quantity']"
