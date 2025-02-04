03f03ca1a9103afc50b8cfdeb033922ecad05f29
justwriteandtest <57027372+justwriteandtest@users.noreply.github.com>
2025-02-04 08:31:39 +0700

    Adjust: change `print()` lines to use `allure.step()` in test_end_to_end.py

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -26,131 +27,106 @@ load_dotenv(override = True)
 
 @pytest.mark.parametrize("itemList, addToCartMethod, removeFromCartMethod, firstName, lastName, postalCode", endToEndData)
 def testEndToEnd(browser : webdriver.Chrome | webdriver.Edge | webdriver.Firefox | webdriver.Safari, itemList, addToCartMethod, removeFromCartMethod,  firstName, lastName, postalCode):
-    login = LoginPage(browser)
-    inventory = InventoryPage(browser)
-    navbar = NavBar(browser)
-    cart = CartPage(browser)
-    details = ItemDetailsPage(browser)
-    shipping = ShippingPage(browser)
-    overview = OverviewPage(browser)
-    complete = CompletePage(browser)
+    with allure.step("Prepare the page classes"):
+        login = LoginPage(browser)
+        inventory = InventoryPage(browser)
+        navbar = NavBar(browser)
+        cart = CartPage(browser)
+        details = ItemDetailsPage(browser)
+        shipping = ShippingPage(browser)
+        overview = OverviewPage(browser)
+        complete = CompletePage(browser)
     
-    username = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")    
+    with allure.step("Log In"):
+        username = os.getenv("USERNAME")
+        password = os.getenv("PASSWORD")    
 
-    login.inputUsername(username)
-    login.inputPassword(password)
-    login.clickLoginButton()
+        login.inputUsername(username)
+        login.inputPassword(password)
+        login.clickLoginButton()
 
-    print("Logging in...")
+        try:
+            WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='app_logo']")))
 
-    try:
-        WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='app_logo']")))
-
-        print(f"Site title: {inventory.siteTitle()}")
-        print(f"Page title: {inventory.pageTitle()}")
-
-        assert inventory.siteTitle() == "Swag Labs"
-        assert inventory.pageTitle() == "Products"
-
-        print("Login successful.")
-    except Exception as e:
-        print("Login failed.")
-
-        print(e)
-
-        assert False
+            assert inventory.siteTitle() == "Swag Labs"
+            assert inventory.pageTitle() == "Products"
+        except Exception as e:
+            assert False
 
     cartCheck = {}
 
-    for item in itemList.keys():
-        print(f"Adding {item} to cart...")
+    with allure.step("Add products to cart"):
+        for item in itemList.keys():
+            match addToCartMethod[item]:
+                case AddToCartMethods.detailsPage:
+                    with allure.step(f"Add {item} to cart from its details page"):
+                        inventory.goToPage(item)
+                        details.addToCart()
+                        details.backToProducts()
 
-        match addToCartMethod[item]:
-            case AddToCartMethods.detailsPage:
-                inventory.goToPage(item)
-                print(f"Entering {item}'s description page...")
+                case AddToCartMethods.inventoryPage:
+                    with allure.step(f"Add {item} to cart from inventory page"):
+                        inventory.addToCart(item)
 
-                details.addToCart()
-                print(f"Adding {item} to the cart...")
-
-                details.backToProducts()
-                print(f"Returning to 'Products' page...")
+                case _:
+                    pass
             
-            case AddToCartMethods.inventoryPage:
-                inventory.addToCart(item)
-                print(f"Adding {item} to cart from inventory page...")
-            
-            case _:
-                pass
-        
-        cartCheck[item] = False
+            cartCheck[item] = False
 
-    for item in itemList.keys():
-        match removeFromCartMethod[item]:
-            case RemoveFromCartMethods.detailsPage:
-                print(f"Removing {item} from cart...")
+    with allure.step("Remove items from cart"):
+        for item in itemList.keys():
+            match removeFromCartMethod[item]:
+                case RemoveFromCartMethods.detailsPage:
+                    with allure.step(f"Remove {item} from cart through its details page"):
+                        inventory.goToPage(item)
+                        details.removeFromCart()
+                        details.backToProducts()
 
-                inventory.goToPage(item)
-                print(f"Entering {item}'s description page.")
+                case RemoveFromCartMethods.inventoryPage:
+                    with allure.step(f"Remove {item} from cart through inventory page"):
+                        inventory.removeFromCart(item)
 
-                details.removeFromCart()
-                print(f"Clicked 'Remove from Cart' button on {item}'s page'.")
-
-                details.backToProducts()
-                print("Returning to 'products' page.")
-            
-            case RemoveFromCartMethods.inventoryPage:
-                print(f"Removing {item} from cart...")
-                inventory.removeFromCart(item)
-                print(f"Clicked 'Remove from Cart' button on {item}'s card.")
-
-            case RemoveFromCartMethods.cartPage:
-                print(f"Removing {item} from cart...")
-                
-                navbar.goToCart()
-                print(f"Entering cart page and searching for {item}'s card...")
-
-                cart.removeItemFromCart(item)
-                print(f"Clicked 'Remove from Cart' button on {item}'s card.")
-
-                cart.continueShopping()
-                print("Returning to 'products' page.")
-            
-            case _:
-                pass        
+                case RemoveFromCartMethods.cartPage:
+                    with allure.step(f"Remove {item} from cart through cart page"):
+                        navbar.goToCart()
+                        cart.removeItemFromCart(item)                    
+                        cart.continueShopping()
+                    
+                case _:
+                    pass     
 
     navbar.goToCart()
 
-    for item in itemList:
-        cartCheck[item] = cart.itemIsInCart(item)
+    with allure.step("Check if the cart contains the expected items"):
+        navbar.goToCart()
 
-    print(cartCheck)
-    print(itemList)
+        for item in itemList:
+            cartCheck[item] = cart.itemIsInCart(item)
 
-    assert cartCheck == itemList
+        assert cartCheck == itemList
 
-    print("Cart checking succesful")
+    with allure.step("Check out: Fill the shipping details"):
+        cart.checkout()
 
-    cart.checkout()
+        shipping.fillFirstName(firstName)
+        shipping.fillLastName(lastName)
+        shipping.fillPostalCode(postalCode)
 
-    shipping.fillFirstName(firstName)
-    shipping.fillLastName(lastName)
-    shipping.fillPostalCode(postalCode)
+    with allure.step("Cancel the check out"):
+        shipping.cancelCheckout()
 
-    shipping.cancelCheckout()
+    with allure.step("Check out: Fill the shipping details again"):
+        navbar.goToCart()
+        cart.checkout()
 
-    navbar.goToCart()
-    cart.checkout()
+        shipping.fillFirstName(firstName)
+        shipping.fillLastName(lastName)
+        shipping.fillPostalCode(postalCode)
 
-    shipping.fillFirstName(firstName)
-    shipping.fillLastName(lastName)
-    shipping.fillPostalCode(postalCode)
-
-    shipping.continueCheckout()
+        shipping.continueCheckout()
 
     #TODO: Assert total price checks
-
-    overview.finishCheckout()
-
-    complete.returnToHome()
+    
+    with allure.step("Finish the checkout process"):
+        overview.finishCheckout()
+        complete.returnToHome()
